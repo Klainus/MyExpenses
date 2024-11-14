41a8ee83ffd1135620a9ef1774e5048d2e1f0833
Klainus <106882227+Klainus@users.noreply.github.com>
2024-11-14 13:59:06 +0100

    Core Data Subclass
    -Created core data subclass to support entity

---
+++ Expense+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Expense+CoreDataClass.swift
+//  MyExpenses
+//
+//  Created by Linus Karlsson on 2024-11-14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Expense)
+public class Expense: NSManagedObject {
+
+}
