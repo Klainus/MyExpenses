db9d691c5b0a1fd263dc820ab96f4147000a5c11
Klainus <106882227+Klainus@users.noreply.github.com>
2024-11-17 21:53:00 +0100

    test
    -tried booting from different files with @main

--- MyExpenses/MyExpensesApp.swift
+++ MyExpenses/MyExpensesApp.swift
@@ -13,7 +13,7 @@ struct MyExpensesApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
